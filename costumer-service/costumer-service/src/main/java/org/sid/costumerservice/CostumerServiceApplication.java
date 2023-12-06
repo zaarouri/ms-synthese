47d11e04cbe05ec514c0d8eed54317169fe0220d
Zaarouri <abdelmounime.zaarouri@gmail.com>
2023-12-06 08:26:08 +0100

    part 1

---
+++ costumer-service/costumer-service/src/main/java/org/sid/costumerservice/CostumerServiceApplication.java
@@ -0,0 +1,14 @@
+package org.sid.costumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CostumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumerServiceApplication.class, args);
+	}
+
+}
